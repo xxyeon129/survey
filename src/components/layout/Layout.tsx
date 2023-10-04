6f42b5be2fb3cca34d0fda46ec544782a72dfcc4
xxyeon129 <xxyeon129@gmail.com>
2023-10-04 15:45:45 +0900

    feat: create Layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,25 @@
+import { useLocation } from 'react-router-dom';
+import { PATH_URL } from 'shared/constants/path.const';
+import Sidebar from 'components/layout/sidebar/Sidebar';
+import Header from './header/Header';
+import './layout.scss';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout(props: LayoutProps) {
+  const location = useLocation();
+  const normalPaths = [...Object.values(PATH_URL), ...Object.values(PATH_URL.SURVEY)];
+  const showSidebar = normalPaths.includes(location.pathname);
+
+  return (
+    <>
+      <Header />
+      <main>
+        {showSidebar && <Sidebar />}
+        <section>{props.children}</section>
+      </main>
+    </>
+  );
+}
