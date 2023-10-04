31cf852b8f0cf7b77023b6a6f630b432843c7405
xxyeon129 <xxyeon129@gmail.com>
2023-10-04 13:41:41 +0900

    style: setting reset.scss, add _variables.scss, vite.config.ts scss setting

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr({ svgrOptions: {} })],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/shared/scss/variables.scss";`,
+      },
+    },
+  },
 });
