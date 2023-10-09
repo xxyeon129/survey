f99fd164787a2616492188b740abc18dcbda746c
xxyeon129 <xxyeon129@gmail.com>
2023-10-09 22:06:23 +0900

    feat/design: create sidebar

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,5 +1,32 @@
+import { SURVEY, SURVEY_TITLE } from 'shared/constants/survey.const';
 import './sidebar.scss';
+import { useState } from 'react';
 
 export default function Sidebar() {
-  return <aside></aside>;
+  const [checkedIndex, setCheckedIndex] = useState(-1);
+
+  const handleClick = (index: number) => {
+    setCheckedIndex(index);
+
+    // TODO: 페이지 이동
+  };
+
+  const surveyList = Object.values(SURVEY).map((el) => el.TYPE);
+
+  return (
+    <aside>
+      <ul>
+        <li>{SURVEY_TITLE}</li>
+        {surveyList.map((surveyType, index) => (
+          <li
+            key={index}
+            className={checkedIndex === index ? 'checked' : ''}
+            onClick={() => handleClick(index)}
+          >
+            {index}. {surveyType}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
