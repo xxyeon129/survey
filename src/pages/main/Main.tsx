b2b2c5595c04edd257cce761cd7749b64ee5887a
xxyeon129 <xxyeon129@gmail.com>
2023-10-12 16:39:20 +0900

    style: main page separate variable code

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,31 +1,8 @@
 import { HOSPITAL_NAME, SURVEY_TITLE } from 'shared/constants/survey.const';
 import styles from './main.module.scss';
-import continueIcon from 'assets/mainpage-continue-icon.svg';
-import createIcon from 'assets/mainpage-create-icon.svg';
-import excelIcon from 'assets/mainpage-excel-icon.svg';
+import { routeItems } from './main.const';
 
 export default function Main() {
-  const routeItems = [
-    {
-      icon: continueIcon,
-      title: '작성한 내용 이어서 작성',
-      description: '해당 페이지에서 작성하신 내용이 있다면\n이어서 작성해주세요.',
-      alt: 'pencil icon',
-    },
-    {
-      icon: createIcon,
-      title: '새로 작성',
-      description: '설문이 처음이시라면 새로 작성해주세요.',
-      alt: 'create file icon',
-    },
-    {
-      icon: excelIcon,
-      title: 'Excel 파일 첨부 후 이어서 작성',
-      description: '기존 작성 내용이 저장된 엑셀 파일이 있다면\n첨부 후 이어서 작성해주세요.',
-      alt: 'excel icon',
-    },
-  ];
-
   return (
     <article className={styles['main-container']}>
       <hgroup className={styles['title-container']}>
@@ -34,7 +11,7 @@ export default function Main() {
       </hgroup>
       <ul className={styles['route-container']}>
         {routeItems.map((routeList) => (
-          <li className={styles['route-box']}>
+          <li className={styles['route-box']} key={routeList.id}>
             <div className={styles['route-box-content']}>
               <figure className={styles['route-box-icon-wrapper']}>
                 <img src={routeList.icon} alt={routeList.alt} />
