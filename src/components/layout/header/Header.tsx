1ff66a9e53bed43c12be5ab4b361e6a8e1c2be0c
xxyeon129 <xxyeon129@gmail.com>
2023-10-12 15:23:47 +0900

    style: header right text static variable conversion, add header className

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,6 +3,7 @@ import logo from 'assets/header-logo.svg';
 import PrevNextBtn from './PrevNextBtn';
 import ProgressBar from './ProgressBar';
 import styles from './header.module.scss';
+import { SURVEY_TITLE } from 'shared/constants/survey.const';
 
 export default function Header() {
   const isSurveyPage = usePathCheck();
@@ -14,11 +15,11 @@ export default function Header() {
       <PrevNextBtn />
     </>
   ) : (
-    <span>이상운동질환 비운동증상 설문</span>
+    <span>{`${SURVEY_TITLE} 전자설문`}</span>
   );
 
   return (
-    <header>
+    <header className={styles['header']}>
       <div className={styles['header-contents']}>
         <img src={logo} alt="header 좌측 병원 로고" />
         <div>{rightContent}</div>
