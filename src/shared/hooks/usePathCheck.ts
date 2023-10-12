07d182589ad9117cf060ead9cfb1c24fa96796ad
xxyeon129 <xxyeon129@gmail.com>
2023-10-12 16:14:14 +0900

    comment: add comment - layout css, main page css, usePatchCheck hook

--- src/shared/hooks/usePathCheck.ts
+++ src/shared/hooks/usePathCheck.ts
@@ -1,6 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import { PATH_URL } from 'shared/constants/path.const';
 
+// for non-survey page
+// sidebar, header right content, header progress bar display
 export default function usePathCheck() {
   const location = useLocation();
   const normalPaths = [...Object.values(PATH_URL), ...Object.values(PATH_URL.SURVEY)];
