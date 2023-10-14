8317fe51243ba6d3468f949a866c7c437c5fde27
xxyeon129 <xxyeon129@gmail.com>
2023-10-14 21:18:03 +0900

    design: survey page title publishing

--- src/pages/survey/common/SurveyTitle.tsx
+++ src/pages/survey/common/SurveyTitle.tsx
@@ -1,3 +1,22 @@
-export default function SurveyTitle() {
-  return <title>TITLE</title>;
+import styles from './surveyTitle.module.scss';
+
+interface SurveyTitleProps {
+  title: string;
+  subTitle?: string;
+}
+
+export default function SurveyTitle(props: SurveyTitleProps) {
+  return (
+    <article className={styles['title-container']}>
+      <header>
+        <div className={styles.circle} />
+        <hgroup>
+          <h1 className={styles.title}>{props.title}</h1>
+          <h3 className={styles['sub-title']}>{props.subTitle}</h3>
+        </hgroup>
+      </header>
+      {/* TO DO: 설명이 있는 설문 종류일 경우 추가, css setting */}
+      {/* <p></p> */}
+    </article>
+  );
 }
--- src/pages/survey/survey-UPDRS/SurveyUPDRS.tsx
+++ src/pages/survey/survey-UPDRS/SurveyUPDRS.tsx
@@ -1,3 +1,11 @@
+import { SURVEY } from 'shared/constants/survey.const';
+import SurveyTitle from '../common/SurveyTitle';
+import styles from 'pages/survey/common/survey.module.scss';
+
 export default function SurveyUPDRS() {
-  return <div>UPDRS I & II</div>;
+  return (
+    <article className={styles['survey-container']}>
+      <SurveyTitle title={SURVEY[1].TITLE} subTitle={SURVEY[1].SUB_TITLE} />
+    </article>
+  );
 }
