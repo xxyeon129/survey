8e8b187d2454189114119d28481744e6a70c4321
xxyeon129 <xxyeon129@gmail.com>
2023-10-17 15:09:27 +0900

    test/design: survey-1-updrs content publishing

--- src/pages/survey/survey-01-UPDRS/Survey01UPDRS.tsx
+++ src/pages/survey/survey-01-UPDRS/Survey01UPDRS.tsx
@@ -1,11 +1,24 @@
 import { SURVEY } from 'shared/constants/survey.const';
 import SurveyTitle from '../common/survey-title/SurveyTitle';
 import styles from 'pages/survey/common/survey.module.scss';
+import useQnAList from '../common/hooks/useQnAList';
+import SurveyContentMedicine from '../common/survey-content/SurveyContentMedicine';
 
 export default function Survey01UPDRS() {
+  const questionList = SURVEY[1].QUESTIONS;
+  const { questionTitle, questionAnswers } = useQnAList(questionList);
+
   return (
     <article className={styles['survey-container']}>
       <SurveyTitle title={SURVEY[1].TITLE} subTitle={SURVEY[1].SUB_TITLE} />
+      {questionTitle.map((question, index) => (
+        <SurveyContentMedicine
+          questionNo={index + 1}
+          question={question}
+          answers={questionAnswers[index]}
+          key={index}
+        />
+      ))}
     </article>
   );
 }
