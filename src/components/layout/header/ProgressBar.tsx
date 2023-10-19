a44835d1e8cbc32232c557507f360db366897dfe
xxyeon129 <xxyeon129@gmail.com>
2023-10-19 10:59:11 +0900

    feat: progress bar filled based on recoil headerPage state

--- src/components/layout/header/ProgressBar.tsx
+++ src/components/layout/header/ProgressBar.tsx
@@ -1,9 +1,16 @@
+import { useRecoilValue } from 'recoil';
 import styles from './header.module.scss';
+import { headerCurrentPageState, headerTotalPageState } from './pagination/headerPageState';
 
 export default function ProgressBar() {
+  const headerTotalPages = useRecoilValue(headerTotalPageState);
+  const headerCurrentPage = useRecoilValue(headerCurrentPageState);
+
+  const getProgressWidth = Math.floor((headerCurrentPage / headerTotalPages) * 100);
+
   return (
     <div className={styles['progress-bar-background']}>
-      <div className={styles['progress-bar-filled']} />
+      <div className={styles['progress-bar-filled']} style={{ width: `${getProgressWidth}%` }} />
     </div>
   );
 }
