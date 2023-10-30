48afe1539eae07e3fe256dc78e3bcd23a9743939
xxyeon129 <xxyeon129@gmail.com>
2023-10-30 22:04:39 +0900

    feat: survey-05-RBD add pre-question

--- src/pages/survey/survey-05-RBD/Survey05RBD.tsx
+++ src/pages/survey/survey-05-RBD/Survey05RBD.tsx
@@ -9,14 +9,16 @@ import {
 import usePagination from '../common/hooks/usePagination';
 // constants
 import { SURVEY_TITLE_LIST } from 'common/constants/survey.const';
-import { RBD_QUESTIONS, RBD_QUESTIONS_PER_PAGE } from './survey.const';
+import { RBD_PRE_QUESTION, RBD_QUESTIONS, RBD_QUESTIONS_PER_PAGE } from './survey.const';
 import { SURVEY_04_BDI_TOTAL_PAGES } from '../survey-04-BDI/survey.const';
 // components
 import SurveyTitle from '../common/components/survey-title/SurveyTitle';
 import SurveyContentWithShortAnswers from '../common/components/survey-contents/survey-contents-with-short-answers/SurveyContent';
 import BottomPrevNextButton from '../common/components/bottom-prev-next-button/BottomPrevNextButton';
+import AnswerList from '../common/components/survey-contents/answerList/AnswerList';
 // styles
 import styles from '../common/survey.module.scss';
+import contentStyles from '../common/components/survey-contents/survey-contents-with-short-answers/surveyContent.module.scss';
 
 export default function Survey05RBD() {
   // pagination hook props
@@ -39,10 +41,8 @@ export default function Survey05RBD() {
   return (
     <article className={styles['survey-container']}>
       <SurveyTitle title={SURVEY_TITLE_LIST[5].TITLE} subTitle={SURVEY_TITLE_LIST[5].SUB_TITLE} />
-
-      {/* TO DO: 설문지 작성자 질문 추가 */}
-
       <ul>
+        <PreQuestion />
         {currentPageQuestions.map((question) => (
           <SurveyContentWithShortAnswers question={question} key={question.No} />
         ))}
@@ -50,5 +50,27 @@ export default function Survey05RBD() {
 
       <BottomPrevNextButton handleNextPage={handleNextPage} handlePrevPage={handlePrevPage} />
     </article>
+  );
+}
+
+function PreQuestion() {
+  return (
+    <li className={contentStyles['questions-li']}>
+      <hr className={styles.hr} />
+      <hgroup className={contentStyles['questions-title']}>
+        <h4>{RBD_PRE_QUESTION.Q}</h4>
+      </hgroup>
+
+      <ul className={contentStyles['answers-ul']}>
+        {RBD_PRE_QUESTION.A.map((answer) => (
+          <AnswerList
+            answer={answer}
+            inputName={'RBD-pre-question'}
+            inputId={`RBD-pre-question-${answer}`}
+            key={`RBD-pre-question-${answer}`}
+          />
+        ))}
+      </ul>
+    </li>
   );
 }
