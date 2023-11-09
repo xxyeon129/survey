57ad4a6f5850d36f704384b5571a739ff352ff8e
xxyeon129 <xxyeon129@gmail.com>
2023-11-09 13:23:41 +0900

    feat: main page excel upload implement

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,23 +1,27 @@
 import { useNavigate } from 'react-router-dom';
-import { routeItems } from './main.const';
+import { v4 as uuidv4 } from 'uuid';
+// states
+import { useSetRecoilState } from 'recoil';
+import { headerCurrentPageState } from 'common/layout/header/pagination/headerPageState';
+// constants
+import { MAIN_PAGE_CONTINUE, MAIN_PAGE_CREATE, MAIN_PAGE_EXCEL, routeItems } from './main.const';
 import { HOSPITAL_NAME, SURVEY_NAME } from 'common/constants/survey.const';
 import { PATH_URL } from 'common/constants/path.const';
+// hooks
+import useExcelFile from 'common/hooks/useExcelFile';
+// styles
 import styles from './main.module.scss';
-import { v4 as uuidv4 } from 'uuid';
 
 export default function MainPage() {
   const navigate = useNavigate();
 
   const handleRouteBoxClick = (id: string) => {
     switch (id) {
-      case 'CONTINUE': // 작성 내용 이어서 작성
+      case MAIN_PAGE_CONTINUE: // 작성 내용 이어서 작성
         onClickContinueBox();
         break;
-      case 'CREATE': // 새로 작성
+      case MAIN_PAGE_CREATE: // 새로 작성
         onClickCreateBox();
-        break;
-      case 'EXCEL': // Excel 파일 첨부 후 이어서 작성
-        onClickExcelBox();
         break;
       default:
         break;
@@ -34,26 +38,25 @@ export default function MainPage() {
     navigate(PATH_URL.PERSONAL);
   };
 
-  const onClickExcelBox = () => {
-    // TO DO: 엑셀 파일 받아서 설문 페이지 전체 적용, 설문 페이지로 이동
-    console.log('Excel 파일 첨부 후 이어서 작성');
-  };
-
-  const routeBoxes = routeItems.map((routeList) => (
+  const routeBoxes = routeItems.map((routeBoxesItem) => (
     <li
       className={styles['route-box']}
       key={uuidv4()}
-      onClick={() => handleRouteBoxClick(routeList.id)}
+      onClick={() => handleRouteBoxClick(routeBoxesItem.id)}
     >
-      <div className={styles['route-box-content']}>
-        <figure className={styles['route-box-icon-wrapper']}>
-          <img src={routeList.icon} alt={routeList.alt} />
-        </figure>
-        <div className={styles['route-box-text-wrapper']}>
-          <h3>{routeList.title}</h3>
-          <div className={styles['route-box-description']}>{routeList.description}</div>
+      {routeBoxesItem.id === MAIN_PAGE_EXCEL ? (
+        <UploadExcelFileBox routeBoxesItem={routeBoxesItem} />
+      ) : (
+        <div className={styles['route-box-content']}>
+          <figure className={styles['route-box-icon-wrapper']}>
+            <img src={routeBoxesItem.icon} alt={routeBoxesItem.alt} />
+          </figure>
+          <div className={styles['route-box-text-wrapper']}>
+            <h3>{routeBoxesItem.title}</h3>
+            <div className={styles['route-box-description']}>{routeBoxesItem.description}</div>
+          </div>
         </div>
-      </div>
+      )}
     </li>
   ));
 
@@ -65,5 +68,40 @@ export default function MainPage() {
       </hgroup>
       <ul className={styles['route-container']}>{routeBoxes}</ul>
     </article>
+  );
+}
+
+function UploadExcelFileBox({ routeBoxesItem }: { routeBoxesItem: { [key: string]: string } }) {
+  const setHeaderCurrentPage = useSetRecoilState(headerCurrentPageState);
+  const navigate = useNavigate();
+  const { uploadExcelFileHandler, fileRef } = useExcelFile();
+
+  const onClickExcelBox = () => {
+    uploadExcelFileHandler();
+    // TO DO: 작성한 부분까지 페이지 이동
+    navigate(PATH_URL.SURVEY['01_UPDRS']);
+    setHeaderCurrentPage(1);
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        accept=".xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        ref={fileRef}
+        id="excel"
+        className={styles['excel-input']}
+        onChange={onClickExcelBox}
+      />
+      <label htmlFor="excel" className={styles['route-box-content']}>
+        <figure className={styles['route-box-icon-wrapper']}>
+          <img src={routeBoxesItem.icon} alt={routeBoxesItem.alt} />
+        </figure>
+        <div className={styles['route-box-text-wrapper']}>
+          <h3>{routeBoxesItem.title}</h3>
+          <div className={styles['route-box-description']}>{routeBoxesItem.description}</div>
+        </div>
+      </label>
+    </>
   );
 }
