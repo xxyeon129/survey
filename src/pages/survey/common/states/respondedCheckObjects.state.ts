e4fd507106a0b634a604c6e26d93404a2c9c7ec2
xxyeon129 <xxyeon129@gmail.com>
2023-11-14 22:43:38 +0900

    feat: add all survey respondedCheckObject state

--- src/pages/survey/common/states/respondedCheckObjects.state.ts
+++ src/pages/survey/common/states/respondedCheckObjects.state.ts
@@ -3,16 +3,89 @@ import { recoilPersist } from 'recoil-persist';
 import { RBD_QUESTIONS } from 'pages/survey/survey-05-RBD/survey.const';
 import { SCOPA_QUESTIONS } from 'pages/survey/survey-10-SCOPA/survey.const';
 import { CONSTIPATION_QUESTIONS } from 'pages/survey/survey-11-CONSTIPATION/survey.const';
+import { BAI_QUESTIONS } from 'pages/survey/survey-03-BAI/survey.const';
+import { TIRED_QUESTIONS } from 'pages/survey/survey-09-TIRED/survey.const';
+import { PDQ_QUESTIONS } from 'pages/survey/survey-07-PDQ/survey.const';
+import { FOOD_QUESTIONS } from 'pages/survey/survey-12-FOOD/survey.const';
+import { UPDRS_QUESTIONS } from 'pages/survey/survey-01-UPDRS/survey.const';
+import { FG_QUESTIONS } from 'pages/survey/survey-02-FG/survey.const';
+import { BDI_QUESTIONS } from 'pages/survey/survey-04-BDI/survey.const';
+import { NMS_QUESTIONS } from 'pages/survey/survey-06-NMS/survey.const';
+import { PDSS_QUESTIONS } from 'pages/survey/survey-08-PDSS/survey.const';
 
 const { persistAtom } = recoilPersist({
   key: 'responded-check-for-not-responded-icon',
   storage: sessionStorage,
 });
 
+export const respondedCheckObject01UPDRS = atom({
+  key: 'responded-check-survey01UPDRS',
+  default: Object.fromEntries(
+    Array.from({ length: UPDRS_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject02FG = atom({
+  key: 'responded-check-survey02FG',
+  default: Object.fromEntries(
+    Array.from({ length: FG_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject03BAI = atom({
+  key: 'responded-check-survey03BAI',
+  default: Object.fromEntries(
+    Array.from({ length: BAI_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject04BDI = atom({
+  key: 'responded-check-survey04BDI',
+  default: Object.fromEntries(
+    Array.from({ length: BDI_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
 export const respondedCheckObject05RBD = atom({
   key: 'responded-check-survey05RBD',
   default: Object.fromEntries(
     Array.from({ length: RBD_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject06NMS = atom({
+  key: 'responded-check-survey06NMS',
+  default: Object.fromEntries(
+    Array.from({ length: NMS_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject07PDQ = atom({
+  key: 'responded-check-survey07PDQ',
+  default: Object.fromEntries(
+    Array.from({ length: PDQ_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject08PDSS = atom({
+  key: 'responded-check-survey08PDSS',
+  default: Object.fromEntries(
+    Array.from({ length: PDSS_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject09Tired = atom({
+  key: 'responded-check-survey09Tired',
+  default: Object.fromEntries(
+    Array.from({ length: TIRED_QUESTIONS.length }, (_, index) => [index + 1, false])
   ),
   effects_UNSTABLE: [persistAtom],
 });
@@ -29,6 +102,14 @@ export const respondedCheckObject11Constipation = atom({
   key: 'responded-check-survey11Constipation',
   default: Object.fromEntries(
     Array.from({ length: CONSTIPATION_QUESTIONS.length }, (_, index) => [index + 1, false])
+  ),
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const respondedCheckObject12Food = atom({
+  key: 'responded-check-survey12Food',
+  default: Object.fromEntries(
+    Array.from({ length: FOOD_QUESTIONS.length }, (_, index) => [index + 1, false])
   ),
   effects_UNSTABLE: [persistAtom],
 });
