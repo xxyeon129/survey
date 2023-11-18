9e3205f71cf1f7315b68e00b5e2a716f843eaaec
xxyeon129 <xxyeon129@gmail.com>
2023-11-19 03:30:15 +0900

    design: redesign header save modal for patient user

--- src/common/layout/header/excelFileHandle/modal/HeaderSaveModalForPatient.tsx
+++ src/common/layout/header/excelFileHandle/modal/HeaderSaveModalForPatient.tsx
@@ -1,52 +1,37 @@
-import { useRecoilValue } from 'recoil';
-// states
-import { userState } from 'pages/select-home/selectHomePage.state';
-// constants
-import { USER_HOSPITAL } from 'pages/select-home/selectHomeUser.const';
 // hooks
 import useExcelFile from 'common/hooks/useExcelFile';
 // styles
+import { FaPaperPlane } from 'react-icons/fa';
 import styles from './HeaderSaveModalForPatient.module.scss';
 
-export default function SendExcelFileModal({ onClose }: { onClose: () => void }) {
+export default function HeaderSaveModalForPatient({ onClose }: { onClose: () => void }) {
   const keepModalOpen = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
   };
 
   const onCloseModal = onClose;
-  const { downloadExcelFileHandler, sendFile } = useExcelFile({ onCloseModal });
-
-  const onClickdownloadExcelFileBtn = () => {
-    downloadExcelFileHandler();
-    onClose();
-  };
-
-  // for patient user - hide excel file download button
-  const user = useRecoilValue(userState);
+  const { sendFile } = useExcelFile({ onCloseModal });
 
   return (
-    <div className={styles['modal']} onClick={onClose}>
-      <div className={styles['modal-content']} onClick={keepModalOpen}>
-        <h3>
-          현재 기기에서 작성하신 내용은 창을 닫아도 임시저장됩니다.
-          <br />
-          다른 기기에서 이어서 작성하실 예정일 경우"작성 내용 병원 전송에 동의" 버튼을 눌러주세요.
-        </h3>
-        <section className={styles['btn-container']}>
-          {user === USER_HOSPITAL && (
-            <button className={styles['download-mail-btn']} onClick={onClickdownloadExcelFileBtn}>
-              작성 내용 엑셀 파일로 다운로드
-            </button>
-          )}
-          <button className={styles['send-mail-btn']} onClick={sendFile}>
-            작성 내용 병원 전송에 동의
-          </button>
-          {/* TO DO: 모달창 리디자인, 닫기 버튼 */}
-          {/* <button type="button" onClick={onClose}>
-            닫기
-          </button> */}
-        </section>
-      </div>
+    <div className={styles['modal-background']} onClick={onClose}>
+      <article className={styles['modal-content']} onClick={keepModalOpen}>
+        <figure className={styles['top-icon-wrapper']}>
+          <FaPaperPlane className={styles['top-icon']} />
+        </figure>
+        <hgroup className={styles['text-container']}>
+          <h1 className={styles['title-text']}>담당자에게 작성 내용 전송</h1>
+          <h3 className={styles['explain-text']}>
+            현재 기기에서 작성하신 내용은
+            <br />
+            창을 닫아도 임시저장됩니다.
+            <br />
+            다른 기기에서 이어서 작성하실 경우 아래 버튼을 눌러주세요.
+          </h3>
+        </hgroup>
+        <button className={styles['send-mail-btn']} onClick={sendFile}>
+          작성 내용 병원 전송에 동의
+        </button>
+      </article>
     </div>
   );
 }
