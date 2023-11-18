06038e4adedf66e4bf483235a6440d462c9c4c71
xxyeon129 <xxyeon129@gmail.com>
2023-11-18 18:06:46 +0900

    design: last page modal for hospital user redesign

--- src/pages/survey/survey-12-FOOD/components/LastPageModal.tsx
+++ src/pages/survey/survey-12-FOOD/components/LastPageModal.tsx
@@ -2,12 +2,13 @@ import { userState } from 'pages/select-home/selectHomePage.state';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 // constants
-import { PATH_URL } from 'common/constants/path.const';
 import { USER_HOSPITAL } from 'pages/select-home/selectHomeUser.const';
 // hooks
 import useExcelFile from 'common/hooks/useExcelFile';
 // styles
 import { FiCheckCircle } from 'react-icons/fi';
+import { IoMdDownload } from 'react-icons/io';
+import { FaPaperPlane } from 'react-icons/fa';
 import styles from './lastPageModal.module.scss';
 
 export default function LastPageModal({ onClose }: { onClose: () => void }) {
@@ -20,9 +21,14 @@ export default function LastPageModal({ onClose }: { onClose: () => void }) {
   const onCloseModal = onClose;
   const { downloadExcelFileHandler, sendFile } = useExcelFile({ onCloseModal });
 
+  // for hospital user
   const onClickdownloadExcelFileBtn = () => {
     downloadExcelFileHandler();
-    navigate(PATH_URL.MAIN);
+  };
+
+  // for hospital user
+  const onClickNaviagteToHomePage = () => {
+    navigate('/');
     onClose();
   };
 
@@ -35,30 +41,50 @@ export default function LastPageModal({ onClose }: { onClose: () => void }) {
         <section className={styles['modal-top-blue-icon-box']}>
           <FiCheckCircle className={styles['check-icon']} />
         </section>
-        <h3 className={styles['modal-text-h3']}>
-          설문 참여에 감사합니다.
-          {/* <br />
-          환자 개인용 기기에서 작성하신 경우 "작성 내용 병원 전송에 동의"을 클릭해 주시기 바랍니다.
-          {user === USER_HOSPITAL && (
-            <>
-              <br />
-              {`작성하신 내용을 기기에 저장하시려면 "작성 내용 엑셀 파일로 다운로드"를 클릭해 주세요.`}
-            </>
-          )} */}
-        </h3>
-        <p className={styles['modal-text-explain-p']}>
-          하단 버튼 클릭 시 담당자에게 메일 전송 후<br />첫 화면으로 이동됩니다.
-        </p>
-        <section className={styles['btn-container']}>
-          {user === USER_HOSPITAL && (
-            <button className={styles['download-mail-btn']} onClick={onClickdownloadExcelFileBtn}>
-              작성 내용 엑셀 파일로 다운로드
+        <h3 className={styles['modal-thanks-title']}>설문 참여에 감사드립니다.</h3>
+        {user === USER_HOSPITAL ? (
+          <section className={styles['hospital-user-download-send-container']}>
+            <button
+              className={styles['hospital-user-download-send-btn']}
+              onClick={onClickdownloadExcelFileBtn}
+            >
+              <figure className={styles['hospital-user-download-send-icon-container']}>
+                <IoMdDownload className={styles['hospital-user-download-send-icon']} />
+              </figure>
+              <h3 className={styles['hospital-user-download-send-h3']}>엑셀 파일로 다운로드</h3>
+              <p className={styles['hospital-user-download-send-p']}>
+                작성하신 설문 내용이
+                <br />
+                엑셀 파일로 다운로드됩니다.
+              </p>
             </button>
-          )}
-          <button className={styles['send-mail-btn']} onClick={sendFile}>
+            <hr />
+            <button className={styles['hospital-user-download-send-btn']} onClick={sendFile}>
+              <figure className={styles['hospital-user-download-send-icon-container']}>
+                <FaPaperPlane className={styles['hospital-user-download-send-icon']} />
+              </figure>
+              <h3 className={styles['hospital-user-download-send-h3']}>작성 내용 병원 전송 동의</h3>
+              <p className={styles['hospital-user-download-send-p']}>
+                작성하신 설문 내용이
+                <br />
+                담당자에게 메일로 전송됩니다.
+              </p>
+            </button>
+          </section>
+        ) : (
+          <p className={styles['patient-modal-text-explain-p']}>
+            하단 버튼 클릭 시 담당자에게 메일 전송 후<br />첫 화면으로 이동됩니다.
+          </p>
+        )}
+        {user === USER_HOSPITAL ? (
+          <button className={styles['hospital-user-home-btn']} onClick={onClickNaviagteToHomePage}>
+            첫 화면으로 이동
+          </button>
+        ) : (
+          <button className={styles['patient-send-mail-btn']} onClick={sendFile}>
             작성 내용 병원 전송에 동의
           </button>
-        </section>
+        )}
       </article>
     </div>
   );
