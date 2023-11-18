d55a06289feb6df1985bad1a43d025b10069fe82
xxyeon129 <xxyeon129@gmail.com>
2023-11-18 16:20:30 +0900

    design: last page modal redesign

--- src/pages/survey/common/hooks/usePagination.ts
+++ src/pages/survey/common/hooks/usePagination.ts
@@ -22,7 +22,6 @@ interface usePaginationProps {
   conditionToSetPrevSurveyFirstPage?: boolean;
   setPrevSurveyFirstPage?: () => void;
 
-  // TO DO: finish page 완성 후 삭제, 컴포넌트 props 전달도 삭제
   // for survey-12-Food last page modal
   onClickLastPageNextBtnHandler?: () => void;
 }
@@ -74,8 +73,7 @@ export default function usePagination(props: usePaginationProps) {
 
     // for last page
     if (headerCurrentPage === totalPagesCount) {
-      // props.onClickLastPageNextBtnHandler && props.onClickLastPageNextBtnHandler();
-      navigate(PATH_URL.FINISH);
+      props.onClickLastPageNextBtnHandler && props.onClickLastPageNextBtnHandler();
       return;
     }
 
--- src/pages/survey/survey-12-FOOD/components/LastPageModal.tsx
+++ src/pages/survey/survey-12-FOOD/components/LastPageModal.tsx
@@ -1,10 +1,14 @@
-import useExcelFile from 'common/hooks/useExcelFile';
-import styles from './lastPageModal.module.scss';
-import { useNavigate } from 'react-router-dom';
-import { PATH_URL } from 'common/constants/path.const';
-import { useRecoilValue } from 'recoil';
 import { userState } from 'pages/select-home/selectHomePage.state';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+// constants
+import { PATH_URL } from 'common/constants/path.const';
 import { USER_HOSPITAL } from 'pages/select-home/selectHomeUser.const';
+// hooks
+import useExcelFile from 'common/hooks/useExcelFile';
+// styles
+import { FiCheckCircle } from 'react-icons/fi';
+import styles from './lastPageModal.module.scss';
 
 export default function LastPageModal({ onClose }: { onClose: () => void }) {
   const keepModalOpen = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -26,19 +30,25 @@ export default function LastPageModal({ onClose }: { onClose: () => void }) {
   const user = useRecoilValue(userState);
 
   return (
-    <div className={styles['modal']} onClick={onClose}>
-      <div className={styles['modal-content']} onClick={keepModalOpen}>
-        <h3>
-          설문이 종료되었습니다.
-          <br />
+    <div className={styles['modal-background']} onClick={onClose}>
+      <article className={styles['modal-content']} onClick={keepModalOpen}>
+        <section className={styles['modal-top-blue-icon-box']}>
+          <FiCheckCircle className={styles['check-icon']} />
+        </section>
+        <h3 className={styles['modal-text-h3']}>
+          설문 참여에 감사합니다.
+          {/* <br />
           환자 개인용 기기에서 작성하신 경우 "작성 내용 병원 전송에 동의"을 클릭해 주시기 바랍니다.
           {user === USER_HOSPITAL && (
             <>
               <br />
               {`작성하신 내용을 기기에 저장하시려면 "작성 내용 엑셀 파일로 다운로드"를 클릭해 주세요.`}
             </>
-          )}
+          )} */}
         </h3>
+        <p className={styles['modal-text-explain-p']}>
+          하단 버튼 클릭 시 담당자에게 메일 전송 후<br />첫 화면으로 이동됩니다.
+        </p>
         <section className={styles['btn-container']}>
           {user === USER_HOSPITAL && (
             <button className={styles['download-mail-btn']} onClick={onClickdownloadExcelFileBtn}>
@@ -48,11 +58,8 @@ export default function LastPageModal({ onClose }: { onClose: () => void }) {
           <button className={styles['send-mail-btn']} onClick={sendFile}>
             작성 내용 병원 전송에 동의
           </button>
-          {/* <button type="button" onClick={onClose}>
-            닫기
-          </button> */}
         </section>
-      </div>
+      </article>
     </div>
   );
 }
