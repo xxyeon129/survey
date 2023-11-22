a1c66bb864b3360e4b576d2040c61b131ec6e685
xxyeon129 <xxyeon129@gmail.com>
2023-11-22 16:10:07 +0900

    fix: when upload excel file survey-06-NMS sidebar click not allowed error fix

--- src/common/layout/sidebar/selectors/survey06NMS_lastPageResponses.ts
+++ src/common/layout/sidebar/selectors/survey06NMS_lastPageResponses.ts
@@ -1,13 +1,35 @@
 import { selector } from 'recoil';
-import { totalScoreState } from 'pages/survey/survey-06-NMS/survey06NMS.state';
+import {
+  NMS_QUESTIONS,
+  NMS_QUESTIONS_PER_PAGE,
+  SURVEY_06_NMS_STATE_KEYWORD,
+  SURVEY_06_NMS_TOTAL_PAGES,
+} from 'pages/survey/survey-06-NMS/survey.const';
+import { responseState } from 'pages/survey/common/states/surveyResponse.state';
 
 export const survey06NMS_lastPageResponses = selector({
   key: 'survey06NMS_lastPageResponses',
   get: ({ get }) => {
-    const totalScore = get(totalScoreState);
     const lastPageResponseList = [];
 
-    totalScore === '-' ? lastPageResponseList.push('') : lastPageResponseList.push(totalScore);
+    const lastPageStartQuestionNumber =
+      (SURVEY_06_NMS_TOTAL_PAGES - 1) * NMS_QUESTIONS_PER_PAGE + 1;
+
+    for (
+      let questionNumber = lastPageStartQuestionNumber;
+      questionNumber <= NMS_QUESTIONS.length;
+      questionNumber++
+    ) {
+      const frequencyResponseValue = get(
+        responseState(`${SURVEY_06_NMS_STATE_KEYWORD}-${questionNumber}빈도`)
+      );
+      const degreeResponseValue = get(
+        responseState(`${SURVEY_06_NMS_STATE_KEYWORD}-${questionNumber}중증도`)
+      );
+
+      lastPageResponseList.push(frequencyResponseValue);
+      lastPageResponseList.push(degreeResponseValue);
+    }
 
     return lastPageResponseList;
   },
