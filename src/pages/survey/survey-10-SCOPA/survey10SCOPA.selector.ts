8ba36e3eb40a1c3e3cd102fbf338a732fb6adb9d
xxyeon129 <xxyeon129@gmail.com>
2023-12-30 01:57:32 +0900

    fix: survey-10-SCOPA last page next button disabled error fix

--- src/pages/survey/survey-10-SCOPA/survey10SCOPA.selector.ts
+++ src/pages/survey/survey-10-SCOPA/survey10SCOPA.selector.ts
@@ -3,6 +3,7 @@ import { responseState } from '../common/states/surveyResponse.state';
 import {
   SCOPA_QUESTIONS,
   SCOPA_QUESTIONS_WITH_INPUT_TITLE_LIST,
+  SURVEY_10_SCOPA_LAST_INPUT_QUESTION_ALPHABET_KEY_LIST,
   SURVEY_10_SCOPA_STATE_KEYWORD,
 } from './survey.const';
 
@@ -26,12 +27,9 @@ export const survey10SCOPA_responseSelector = selector({
 
     // for question number 26 input questions
     for (let j = 0; j < SCOPA_QUESTIONS_WITH_INPUT_TITLE_LIST.length; j++) {
+      const questionNumber26KeyAlphabet = SURVEY_10_SCOPA_LAST_INPUT_QUESTION_ALPHABET_KEY_LIST[j];
       responseList.push(
-        get(
-          responseState(
-            `${SURVEY_10_SCOPA_STATE_KEYWORD}-26-${SCOPA_QUESTIONS_WITH_INPUT_TITLE_LIST[j]}`
-          )
-        )
+        get(responseState(`${SURVEY_10_SCOPA_STATE_KEYWORD}-26${questionNumber26KeyAlphabet}`))
       );
     }
 
