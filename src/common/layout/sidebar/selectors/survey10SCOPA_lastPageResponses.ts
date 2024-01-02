2589462cc530800361b9a88a868167ceaded8037
xxyeon129 <xxyeon129@gmail.com>
2024-01-02 23:52:26 +0900

    fix: survey-10-SCOPA sidebar click error fix

--- src/common/layout/sidebar/selectors/survey10SCOPA_lastPageResponses.ts
+++ src/common/layout/sidebar/selectors/survey10SCOPA_lastPageResponses.ts
@@ -1,38 +1,35 @@
 import { selector } from 'recoil';
 import { responseState } from 'pages/survey/common/states/surveyResponse.state';
 import {
-  SCOPA_QUESTIONS,
-  SCOPA_QUESTIONS_PER_PAGE,
-  SCOPA_QUESTIONS_WITH_INPUT_TITLE_LIST,
+  SURVEY_10_SCOPA_FEMALE_LAST_PAGE_QUESTION_NUMBER_LIST,
+  SURVEY_10_SCOPA_MALE_LAST_PAGE_QUESTION_NUMBER_LIST,
   SURVEY_10_SCOPA_STATE_KEYWORD,
 } from 'pages/survey/survey-10-SCOPA/survey.const';
+import { personalInfoGenderState } from 'pages/survey/personalInfo/personalInfo.state';
+import { FEMALE } from 'pages/survey/personalInfo/components/rightSection/genderCheck/genderCheckSection.const';
 
 export const survey10SCOPA_lastPageResponses = selector({
   key: 'survey10SCOPA_lastPageResponses',
   get: ({ get }) => {
     const lastPageResponseList = [];
 
-    const lastPageStartQeustionNumber =
-      SCOPA_QUESTIONS.length - (SCOPA_QUESTIONS.length % SCOPA_QUESTIONS_PER_PAGE) + 1;
+    const selectedGender = get(personalInfoGenderState);
+
+    let questionNumberList = SURVEY_10_SCOPA_MALE_LAST_PAGE_QUESTION_NUMBER_LIST;
+
+    if (selectedGender === FEMALE) {
+      questionNumberList = SURVEY_10_SCOPA_FEMALE_LAST_PAGE_QUESTION_NUMBER_LIST;
+    }
 
     for (
-      let questionNumber = lastPageStartQeustionNumber;
-      questionNumber <= SCOPA_QUESTIONS.length;
-      questionNumber++
+      let questionNumberIndex = 0;
+      questionNumberIndex < questionNumberList.length;
+      questionNumberIndex++
     ) {
-      if (questionNumber < SCOPA_QUESTIONS.length) {
-        const responseValue = get(
-          responseState(`${SURVEY_10_SCOPA_STATE_KEYWORD}-${questionNumber}`)
-        );
-        lastPageResponseList.push(responseValue);
-      } else if (questionNumber === SCOPA_QUESTIONS.length) {
-        SCOPA_QUESTIONS_WITH_INPUT_TITLE_LIST.forEach((questionName) => {
-          const lastQuestionResponseValue = get(
-            responseState(`${SURVEY_10_SCOPA_STATE_KEYWORD}-${questionNumber}-${questionName}`)
-          );
-          lastPageResponseList.push(lastQuestionResponseValue);
-        });
-      }
+      const localStorageState = get(
+        responseState(`${SURVEY_10_SCOPA_STATE_KEYWORD}-${questionNumberList[questionNumberIndex]}`)
+      );
+      lastPageResponseList.push(localStorageState);
     }
 
     return lastPageResponseList;
